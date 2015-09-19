c6c93237010dbb7c5417c15eeb03d1919f3a28b0
kamevor1 <kamevor@luc.edu>
2015-09-18 21:57:37 -0500

    .

--- Testing/testing.py
+++ Testing/testing.py
@@ -1,8 +1,17 @@
 __author__ = 'kofia_000'
 import csv
-
+# this command will open the CVS file
 f = open('chicago_High_school_progress_report.csv')
 csv_f = csv.reader(f)
 
 for row in csv_f:
   print (row)
+
+# This command will list the name of the schools with the 4-Year and 5-Year graduation rates
+import csv
+
+f = open('chicago_High_school_progress_report.csv')
+csv_f = csv.reader(f)
+
+for row in csv_f:
+  print (row[1], row[2], row[3])
